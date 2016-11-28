d00e6dc6c8d4b73583ad556b0b6f5c3511cab2ba
Jeroen Valcke <valcke_jeroen@hotmail.com>
2016-11-28 10:05:32 +0100

    bump version

--- public/app/controllers/forms-detail.controller.js
+++ public/app/controllers/forms-detail.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('forms_0.0.3.controllers')
+angular.module('forms_0.1.0.controllers')
     .controller('formsDetailController', [
         '$scope',
         '$rootScope',
